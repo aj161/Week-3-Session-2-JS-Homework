69e0cd47bd3820efd4af2dddf6caece6ac98877a
aj161 <ajermasenoka@gmail.com>
2022-10-13 22:08:37 +0100

    Initial upload

---
+++ app.js
@@ -0,0 +1,56 @@
+
+//Ex 1
+let age = 30;
+console.log(age);
+
+
+//Ex 2
+let numberOfRabbits = 2;
+numberOfRabbits = numberOfRabbits * 3;
+
+console.log(numberOfRabbits);
+
+//Ex 3
+let bodmasExample = 3;
+bodmasExample = (bodmasExample+5)/6;
+
+console.log(bodmasExample);
+
+//Ex 4
+let randomNum = 6*Math.random();
+console.log(randomNum);
+
+//Ex 1.1
+
+let myCountry = "UK"
+console.log(myCountry);
+
+//Ex 2.1
+console.log(myCountry.length);
+
+//Ex 3.1
+let favouriteFood = "sushi";
+favouriteFood = favouriteFood.toUpperCase();
+
+console.log(favouriteFood);
+
+//Ex 4.1
+
+favouriteFood = favouriteFood.toLowerCase();
+console.log(favouriteFood);
+
+//Ex 5
+
+let myName = 'Ana';
+let myAge = 30;
+let message = `Hello my name is ${myName} and I am ${myAge} years old.`
+let appendix = "What is your name?";
+
+console.log (appendix + ' ' + message);
+
+//Ex 6
+
+let firstName = "Anastasija";
+let lastName = 'Jermasenoka';
+
+console.log(`Welcome ${firstName} ${lastName}, How can I help you?`);
